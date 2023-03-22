3a37ad8e0067c0af292b7dd0cef887761923bfd3
laknoch <49613306+hassanlakhal@users.noreply.github.com>
2023-03-22 23:56:06 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,32 +1,20 @@
-#include"main.h"
+#include "main.h"
 /**
-* main - print _putchar ft_putstr
-* str is string
-* is written by mbah
-* Return: 0
-*/
-/**
-* ft_putstr - writes a string to standard output
-* @str: the string to be written
-*
-* This function writes the string pointed to by 'str' to the standard output stream.
-* If the string contains null characters, the output will be truncated at the first null
-* character encountered. There is no return value.
-*/
-void	ft_putstr(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
 
 int main(void)
 {
-	ft_putstr("_putchar\n");
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
