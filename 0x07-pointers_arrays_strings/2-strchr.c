0239718e02417078d0462d87f898a5bc184c48f6
hassanlakhal <lakehalhassan2120@gmail.com>
2023-04-05 01:47:16 +0000

    abcd

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = -1;
+	int i = 0;
 
-	while (++i && s[i])
+	while (s[i])
 	{
 		if (s[i] == c)
 			return (s + i);
+        i++;
 	}
 	return (0);
 }
