1a9e9276fb08887fdf862e7650ebc995e46d7863
laknoch <49613306+hassanlakhal@users.noreply.github.com>
2023-04-19 18:19:49 +0000

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,17 +12,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL)
-		return (-1);
+	i = 0;
 	if (size <= 0)
 		return (-1);
-	if (cmp == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if(array && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		while (i < size)
+		{
+			if(cmp(array[i]) != 0)
+				return i;
+			i++;
+		}
 	}
 	return (-1);
 }
